381435c188d5292972f536f23968aa406a88116e
les <les.lee@slotfactory.com>
2019-12-18 18:51:10 +0800

    add normalize.css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import router from './router';
 import store from './store';
 
 // @ is an alias to /src
+import '@/styles/normalize.css';
 import '@/styles/index.scss';
 
 Vue.config.productionTip = false;
